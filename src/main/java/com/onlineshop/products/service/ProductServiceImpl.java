f7913770aff883e07895ca0683da251bcd150939
UAValchyMa <mvalchyshen@gmail.com>
2021-07-06 19:36:54 +0300

    Added: Validation Changed: ProductServiceImpl

--- src/main/java/com/onlineshop/products/service/ProductServiceImpl.java
+++ src/main/java/com/onlineshop/products/service/ProductServiceImpl.java
@@ -3,11 +3,12 @@ package com.onlineshop.products.service;
 import com.onlineshop.products.model.Product;
 import com.onlineshop.products.repository.BaseRepository;
 import com.onlineshop.products.repository.RepositoryFactory;
+import com.onlineshop.products.util.ValidationUtil;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
     BaseRepository<Product, String> productRepository;
 
@@ -17,8 +18,9 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public Product createNewProduct(String id, Double price, Long saleQuantity, Double salePrice) {
-        if (validPrice(price) && validPrice(salePrice) && validQuantity(saleQuantity)) {
-            return new Product(id,price,saleQuantity,salePrice);
+        if (ValidationUtil.validPrice(price) && ValidationUtil.validPrice(salePrice)
+                && ValidationUtil.validQuantity(saleQuantity) && ValidationUtil.validId(id)) {
+            return new Product(id, price, saleQuantity, salePrice);
         }
         throw new RuntimeException("Price and Quantity must more or equal 0");
 
@@ -26,7 +28,7 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public Product createNewProduct(String id, Double price) {
-        if (validPrice(price)){
+        if (ValidationUtil.validPrice(price) && ValidationUtil.validId(id)) {
             return new Product(id, price);
         }
         throw new RuntimeException("Price must more or equal 0");
@@ -62,14 +64,6 @@ public class ProductServiceImpl implements ProductService{
         productRepository.deleteById(id);
     }
 
-    private boolean validQuantity(Long quantity) {
-        return quantity > 0L;
-    }
-
-    private boolean validPrice(Double price) {
-        return price >= 0D;
-    }
-
 
     @Override
     public Double calculateTotalCost(String products) {
@@ -78,7 +72,7 @@ public class ProductServiceImpl implements ProductService{
                 .filter(Character::isAlphabetic)
                 .mapToObj(x -> productRepository.get(String.valueOf((char) x).toUpperCase(Locale.ROOT)))
                 .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-        for (Map.Entry<Product, Long> entry:map.entrySet()) {
+        for (Map.Entry<Product, Long> entry : map.entrySet()) {
             if (entry.getKey().getSaleQuantity() <= entry.getValue()) {
                 totalCost += entry.getKey().getSalePrice();
             } else {
--- src/main/java/com/onlineshop/products/view/ViewImpl.java
+++ src/main/java/com/onlineshop/products/view/ViewImpl.java
@@ -3,6 +3,9 @@ package com.onlineshop.products.view;
 public class ViewImpl implements View {
 
     public static final String INPUT_PRODUCT_NAME = "Write Product Name";
+    public static final String INPUT_PRODUCT_PRICE = "Write Product Price";
+    public static final String INPUT_PRODUCT_SALE_PRICE = "Write Product Sale Price";
+    public static final String INPUT_PRODUCT_SALE_QUANTITY = "Write Product Sale Quantity";
 
     @Override
     public void printMessage(String message) {
